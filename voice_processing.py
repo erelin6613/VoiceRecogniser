a050a52feffac089621b0d33d900a36cafc12d56
Valentine <46331217+erelin6613@users.noreply.github.com>
2020-03-17 19:21:54 +0200

    Working checkpoint (working as not intended)

--- voice_processing.py
+++ voice_processing.py
@@ -43,23 +43,24 @@ I would be grateful for any suggestion or help.
 import pandas as pd
 from scipy.io import wavfile
 import os
+import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 from keras_preprocessing.sequence import pad_sequences
 from transformers import BertModel, BertConfig, BertTokenizer
 from torch.utils.data import Dataset, DataLoader
-from torch import Tensor, tanh, flatten, squeeze, mean
+from torch import Tensor, tanh, flatten, squeeze, mean, stft
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import Adam
 from torch.nn.utils.rnn import pad_sequence
 from torchaudio import load
 from torchaudio.transforms import MFCC, Resample
+from torchaudio.functional import istft
 import numpy as np
 
 data_dir = '../common-voice'
 audio_dir = 'cv-valid-train'
-#/cv-valid-train'
 sound_len = 10
 bert_path = '../bert-base-uncased'
 bert_vocab_path = 'vocab.txt'
@@ -74,7 +75,7 @@ class VoiceInstance:
         self.file = file
         self.text = text
         self.tokenizer = tokenizer
-        self.mfcc = self._transform_audio()
+        self._transform_audio()
         self.emb = self._get_embeddings()
         
     @staticmethod
@@ -86,23 +87,18 @@ class VoiceInstance:
         waveform, rate = load(os.path.join(data_dir, self.file))
         new_rate = rate/100
         resampled = Resample(rate, new_rate)(waveform)
-        self.fft, self.frequency = self._get_fft(resampled, new_rate)
-        return self._get_mfcc(resampled, new_rate)
+        self.stft = self._get_stft(resampled, new_rate)
+        self.mfcc = self._get_mfcc(resampled, new_rate)
         
     def _get_mfcc(self, arr, sample_rate=22000):
         
-        mfcc_tensor = MFCC(sample_rate)
+        mfcc_tensor = MFCC(sample_rate, n_mfcc=window)
         return mfcc_tensor.forward(arr)
     
-    def _get_fft(self, waveform, rate):
-        
-        length = len(waveform)
-        frequency = np.fft.rfftfreq(length)
-        fft = np.fft.rfft(waveform)/length
-        return fft, frequency
+    def _get_stft(self, waveform, rate):
+        return stft(waveform, int(rate))
     
     def _get_embeddings(self):
-        configuration = BertConfig()
         
         def remove_punkts(text):
             
@@ -148,39 +144,43 @@ class VoiceDataset(Dataset):
                                       text=self.info_frame.loc[i, 'text'], tokenizer=self.tokenizer)
                 self.instances.append(audio)
         embs = []
+        mfccs = []
         for each in self.instances:
             embs.append(each.emb)
+            mfccs.append(each.mfcc)
         embs = pad_sequences(embs, maxlen=max_len_text, padding='pre', value=0)
         for i in range(len(self.instances)):
             self.instances[i].emb = embs[i]
 
-
+        
 class VoiceModel(nn.Module):
     
     def __init__(self):
         
         super().__init__()
+        self.lstm_enc = nn.LSTM(input_size=window, hidden_size=512, batch_first=True)
+        self.lstm_dec = nn.LSTM(input_size=512, hidden_size=256)
         self.input = nn.GRU(input_size=window, bidirectional=True, hidden_size=512)
         self.gru = nn.GRU(input_size=512*2, bidirectional=False, hidden_size=512)
         self.flatten = nn.Flatten()
-        #self.pool = nn.MaxPool1d(10, 5)
-        self.dense = nn.Linear(512, 256)
+        self.dense = nn.Linear(256, 128)
         self.dropout = nn.Dropout(0.3)
-        self.output = nn.Linear(256, max_len_text)
+        self.linear_2 = nn.Linear(128, max_len_text)
+        self.output = nn.Linear(max_len_text*100, 1)
     
     def forward(self, audio):
-        audio = Tensor(audio).reshape(1, audio.shape[2], window)
-        audio, hidden_gru = self.input(audio)
+        audio = Tensor(audio)
+        audio, hidden_enc = self.lstm_enc(audio.reshape(1, -1, window))
         audio = tanh(audio)
-        audio, hidden_out = self.gru(audio) #(audio.view(-1, audio.shape[0], audio.shape[1]))
+        audio, hidden_dec = self.lstm_dec(audio)
         audio = tanh(audio)
-        #audio = self.pool(audio)
-        print(audio.shape)
-        #audio = self.flatten(audio).view(512, -1)
-        #print(audio.shape)
         audio = F.relu(self.dense(audio))
-        audio = F.relu(self.dropout(audio))
+        audio = self.flatten(audio)
+        # that's to see if it works, later I will figure an appropriate value
+        max_sound_len = 3000
+        audio = nn.functional.pad(audio, (max_sound_len-audio.shape[-1], 0), mode='constant', value=0)
         audio = self.output(audio)
+        print(audio.shape)
         
         return audio
 
@@ -190,7 +190,6 @@ def main():
     epochs = 10
     model = VoiceModel()
     criterion = nn.CosineSimilarity(dim=0)
-    criterion.requres_grad = True
     optimizer = Adam(model.parameters(), lr=0.001)
     info_frame = pd.read_csv(os.path.join(data_dir, 'cv-valid-train.csv'))[:50]	# .loc[25:75, :]
     
@@ -204,7 +203,6 @@ def main():
         lengths.append(len(real_texts[i]))
         
     print(info_frame.columns)
-    
     dataset = VoiceDataset(info_frame=info_frame, audio_dir=audio_dir, tokenizer=tokenizer)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     
@@ -215,16 +213,11 @@ def main():
             
             optimizer.zero_grad()
             output = model(batch.mfcc)
-            loss = criterion(output, Tensor(batch.emb))
-            
-            loss.mean().backward()
-            optimizer.step()
             i += 1
-        print(loss.mean())
         
     model.eval()
     
-    test_frame = pd.read_csv(os.path.join(data_dir, 'cv-valid-train.csv'))[51:75]
+    test_frame = pd.read_csv(os.path.join(data_dir, 'cv-valid-train.csv'))[50:75]
     test_frame.reset_index()
     print(test_frame.index)
     
